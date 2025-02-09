b5cca79d58e5b27a9be584e9687b1a2835675638
aachalshres <aachalshrsth@gmail.com>
2025-02-09 09:04:42 +0545

    media

---
+++ example.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div> I am div
+    <button>Login</button>
+
+
+    <div style:"min-height: 200px; position: absolute"> I am div
+    </div>
+    </div>
+
+
+
+    <button>Login</button>
+    <style>
+        button{
+            position: absolute;
+        top: 50%;
+        left: 50%;
+        }
+        div{
+            position: relative;
+        }
+    </style>
+</html>
